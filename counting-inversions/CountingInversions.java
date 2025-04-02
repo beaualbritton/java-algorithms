cb285b6025d5df69e1dbee6af7e1122c29a0d367
beaualbritton <albrittoncb@gmail.com>
2025-04-02 17:38:05 -0400

    merge sort and SortCountResult class

--- counting-inversions/CountingInversions.java
+++ counting-inversions/CountingInversions.java
@@ -4,16 +4,16 @@ public class CountingInversions
 {
     public static void main(String[] args) {
 
-        int [] L = {1,13,14,2,25,26};
-
-        System.out.println(sortAndCount(L));
+        int [] L = {1,5,4,8,10,2,6,9,12,11,3,7};
+        SortCountResult sorted = sortAndCount(L);
+        System.out.println(sorted.count);
     }
 
-    public static int sortAndCount(int[] L)
+    public static SortCountResult sortAndCount(int[] L)
     {
-        //one member in L
+        //one member in L, return 0
         if(L.length == 1)
-            return 0;
+            return new SortCountResult(0, L);
 
         //generating new lists
         int [] A, B;
@@ -21,12 +21,14 @@ public class CountingInversions
         //checking if L.length is uneven. if so, lefthand gets 'shorter end' of the stick. or list in this case :^)
         if (L.length % 2 == 0)
         {
+            //same length
             A = new int[L.length/2];
             B = new int[L.length/2];
 
         }
         else
         {
+            //B gets length/2 + 1. e.g., if length of list is 5, then length of A is 2 while B's is 3
             A = new int[L.length/2];
             B = new int[(L.length/2) + 1];
         }   
@@ -37,35 +39,42 @@ public class CountingInversions
             A[i] = L[i];
         }
         int j;
+        //J is the last index that the prior loop terminates at. So, j = length of A
         for(j = A.length; j < L.length; ++j)
         {
+            //Now subtracting A's length for proper indexing
             B[j-A.length] = L[j];
         }
+
+        //Just printing so I can trace
         printArray(A);
         printArray(B);
-        int ra  = sortAndCount(A);
-        int rb = sortAndCount(B);
-        int a = mergeAndCount(A, B);
 
+        //Inversion counts from next recursive call
+        SortCountResult ra  = sortAndCount(A);
+        SortCountResult rb = sortAndCount(B);
+        SortCountResult r = mergeAndCount(ra.sortedArray, rb.sortedArray);
 
-        return ra+rb+a;
+        //returning r + ra + rb (TODO: RETURN SORTED LIST FROM MERGE&COUNT)
+        return new SortCountResult(ra.count+rb.count+r.count,r.sortedArray);
     }
     
-    public static int mergeAndCount(int[] A, int[] B)
+    public static SortCountResult mergeAndCount(int[] A, int[] B)
     {
-        int[] L;
+        int[] L = new int[A.length + B.length];
         int inversionCount = 0;
         //inversion count 
         for (int i = 0; i < A.length; ++i)
         {
             int currentNumA = A[i];
+            //Comparing if index at A[i] is Greater than B[j]
             for (int j = 0; j < B.length; ++j)
             {
                 int currentNumB = B[j];
                 //System.out.println(currentNumB);
                 if(currentNumA > currentNumB)
                 {
-                    System.out.println(currentNumA + " " + currentNumB);
+                    //System.out.println(currentNumA + " " + currentNumB);
                     ++inversionCount;
                 }
 
@@ -73,10 +82,40 @@ public class CountingInversions
         }
 
         //TODO: implement merge sort x
+        //looping variables 
+        int i = 0,j = 0, k = 0;
+        while(i < A.length && j < B.length)
+        {
+            if(A[i] <= B[j])
+            {
+                L[k] = A[i];
+                ++k; ++i;
+            }
+            else
+            {
+                L[k] = B[j];
+                ++k; ++j;
+                //TODO: COUNT INVERSIONS MORE EFFICIENTLY 
 
-        return inversionCount;
+            }
+        }
+
+        //Now there is at least one element in each arrays A, B that hasn't been added to new list L
+        while (i < A.length)
+        {
+            L[k] = A[i];
+            ++k; ++i;
+        }
+        while (j < B.length)
+        {
+            L[k] = B[j];
+            ++k; ++j;
+        }
+        printArray(L);
+
+        return new SortCountResult(inversionCount, L);
     }
-    
+
     public static void printArray(int[] arr)
     {
         System.out.print("{");
@@ -87,7 +126,7 @@ public class CountingInversions
         System.out.print("}\n");
     }
 }
-
+//Loose data structure that returns (int, int[])
 class SortCountResult
 {
     int count;
